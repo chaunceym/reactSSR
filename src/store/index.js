fb3576978b96102ace13de0f4774ef6ed341ea14
chauncey <18322276773@163.com>
2021-04-21 16:39:06 +0800

    feat: 防止两端得到同一个store

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,7 @@ const reducer = combineReducers({
   home: homeReducer,
 });
 
-const store = createStore(reducer, applyMiddleware(thunk));
-
-export default store;
+// 为了防止导出单例的 store, 每个函数执行会得到不一样的 store
+export default () => {
+  return createStore(reducer, applyMiddleware(thunk));
+};
