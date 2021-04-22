889712bee4f8dadb995300df5dc2e35c4e4b1107
chauncey <18322276773@163.com>
2021-04-22 16:53:06 +0800

    feat: 替换componentWillMount再constructor上执行

--- src/common/withStyle.js
+++ src/common/withStyle.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 
 export default (DecoratedComponent, styles) => {
   return class NewComponent extends Component {
-    componentWillMount() {
-      this.props.staticContext?.css.push(styles._getCss());
+    constructor(props) {
+      super(props);
+      props.staticContext?.css.push(styles._getCss());
     }
+
     render() {
       return <DecoratedComponent {...this.props} />;
     }
